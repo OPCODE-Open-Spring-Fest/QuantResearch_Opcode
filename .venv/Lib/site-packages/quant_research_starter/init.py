fe9b3677026850743e5006b9c682026f309358ba
AYUSH KUMAR TIWARI <139953157+ayushkrtiwari@users.noreply.github.com>
2025-11-19 11:46:57 +0530

    chore: remove venv from repo

--- .venv/Lib/site-packages/quant_research_starter/init.py
+++
@@ -1,8 +0,0 @@
-"""Quantitative Research Starter Package."""
-
-__version__ = "0.1.0"
-__author__ = "QuantResearchStarter Contributors"
-
-from . import backtest, data, factors, metrics, universe
-
-__all__ = ["data", "factors", "universe", "backtest", "metrics"]
